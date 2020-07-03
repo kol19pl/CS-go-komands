9629959d7ac24410c6eb8a2827653600d49607c4
kol19pl <kol19pl@gmail.com>
2020-07-03 22:48:34 +0200

    popapy noży
    Took 2 hours 11 minutes

--- app/src/main/java/com/koltech/komendydocsgo/bronie.java
+++ app/src/main/java/com/koltech/komendydocsgo/bronie.java
@@ -1,7 +1,12 @@
 package com.koltech.komendydocsgo;
 
+import android.app.AlertDialog;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,6 +37,137 @@ public class bronie extends AppCompatActivity {
         setContentView(R.layout.noze);
     }
 
+    public void NozeBulider(View w){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(bronie.this);
+        View mView = getLayoutInflater().inflate(R.layout.noze_pop_up, null);
+        TextView wiad = (TextView) mView.findViewById(R.id.Nozeinfo);
+        ImageView imageView = (ImageView) mView.findViewById(R.id.imagenuz);
+        AdView mxAdView = (AdView) mView.findViewById(R.id.adViewnuzinfo);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mxAdView.loadAd(adRequest);
+
+
+        String comenda=w.getTag().toString();
+
+        Drawable drawable = null;
+        //drawable = getResources().getDrawable(R.drawable.knife_t);
+        if(comenda.contains("knife")){
+            drawable = getResources().getDrawable(R.drawable.knife);
+
+        }
+        if(comenda.contains("knife_t")){
+            drawable = getResources().getDrawable(R.drawable.knife_t);
+
+        }
+        if(comenda.contains("knife_css")){
+            drawable = getResources().getDrawable(R.drawable.css);
+
+        }
+        if(comenda.contains("bayonet")){
+            drawable = getResources().getDrawable(R.drawable.bayonet);
+
+        }
+        if(comenda.contains("knife_flip")){
+            drawable = getResources().getDrawable(R.drawable.flip);
+
+        }
+        if(comenda.contains("knife_gut")){
+            drawable = getResources().getDrawable(R.drawable.gut);
+
+        }
+        if(comenda.contains("knife_karambit")){
+            drawable = getResources().getDrawable(R.drawable.karambit);
+
+        }
+        if(comenda.contains("knife_m9")){
+            drawable = getResources().getDrawable(R.drawable.m9_bayonet);
+
+        }
+        if(comenda.contains("knife_tactical")){
+            drawable = getResources().getDrawable(R.drawable.tactical);
+
+        }
+        if(comenda.contains("knife_butterfly")){
+            drawable = getResources().getDrawable(R.drawable.baterfly);
+
+        }
+        if(comenda.contains("knife_falchion")){
+            drawable = getResources().getDrawable(R.drawable.falchion);
+
+        }
+        if(comenda.contains("knife_push")){
+            drawable = getResources().getDrawable(R.drawable.push);
+
+        }
+        if(comenda.contains("knifegg")){
+            drawable = getResources().getDrawable(R.drawable.knifegg);
+
+        }
+        if(comenda.contains("knife_survival_bowie")){
+            drawable = getResources().getDrawable(R.drawable.survival_bowie);
+
+        }
+        if(comenda.contains("knife_ursus")){
+            drawable = getResources().getDrawable(R.drawable.ursus);
+
+        }
+        if(comenda.contains("knife_gypsy_jackknife")){
+            drawable = getResources().getDrawable(R.drawable.jackknife);
+
+        }
+        if(comenda.contains("knife_stiletto")){
+            drawable = getResources().getDrawable(R.drawable.stiletto);
+
+        }
+        if(comenda.contains("knife_widowmaker")){
+            drawable = getResources().getDrawable(R.drawable.widolmakier);
+
+        }
+        if(comenda.contains("knife_ghost")){
+            drawable = getResources().getDrawable(R.drawable.gost);
+
+        }
+        if(comenda.contains("knife_canis")){
+            drawable = getResources().getDrawable(R.drawable.canis);
+
+        }
+        if(comenda.contains("knife_cord")){
+            drawable = getResources().getDrawable(R.drawable.cord);
+
+        }
+        if(comenda.contains("knife_skeleton")){
+            drawable = getResources().getDrawable(R.drawable.skeleton);
+
+        }
+        if(comenda.contains("knife_outdoor")){
+            drawable = getResources().getDrawable(R.drawable.outdoor);
+
+        }
+
+        imageView.setImageDrawable(drawable);
+
+
+
+
+
+
+       // Button ok = (Button) mView.findViewById(R.id.powiadmienieok);
+        try {
+
+
+        wiad.setText(comenda+"; ");
+        } catch (Exception e) {
+            wiad.setText("error");
+            e.printStackTrace();
+        }
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+
+        dialog.show();
+
+    }
+
    public void Pistolety(View w){
         setContentView(R.layout.pistolety);
     }
@@ -49,6 +185,10 @@ public class bronie extends AppCompatActivity {
     }
     public void Ekwipunek(View v){
         setContentView(R.layout.ekwipunek);
+    }
+
+    public void DangerZone(View v){
+        setContentView(R.layout.danger_zone);
     }
 
     public void Inne(View v){
