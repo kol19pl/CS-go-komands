9a0e2237d8e2bbbbc5471a02d4dd8c93b718971a
kol19pl <kol19pl@gmail.com>
2021-03-30 01:32:18 +0200

    aktualizacja sdk

--- app/src/main/java/com/koltech/komendydocsgo2/ZarzondzanieSerwerem.java
+++ app/src/main/java/com/koltech/komendydocsgo2/ZarzondzanieSerwerem.java
@@ -94,23 +94,18 @@ public class ZarzondzanieSerwerem extends AppCompatActivity {
     }
 
     private AdSize getAdSize() {
-        // Determine the screen width (less decorations) to use for the ad width.
+        // Step 2 - Determine the screen width (less decorations) to use for the ad width.
         Display display = getWindowManager().getDefaultDisplay();
         DisplayMetrics outMetrics = new DisplayMetrics();
         display.getMetrics(outMetrics);
 
+        float widthPixels = outMetrics.widthPixels;
         float density = outMetrics.density;
 
-        float adWidthPixels = adContainerView.getWidth();
+        int adWidth = (int) (widthPixels / density);
 
-        // If the ad hasn't been laid out, default to the full screen width.
-        if (adWidthPixels == 0) {
-            adWidthPixels = outMetrics.widthPixels;
-        }
-
-        int adWidth = (int) (adWidthPixels / density);
-
-        return AdSize.getCurrentOrientationBannerAdSizeWithWidth(this, adWidth);
+        // Step 3 - Get adaptive ad size and return for setting on the ad view.
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
     }
 
 }
